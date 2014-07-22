30541ae7846db53806ed7f204a757ed79ab1cee3
Torrent Lee <torrent88410@gmail.com>
2014-07-22 14:29:05 +0800

    optimize user select

--- libs/drag.js
+++ libs/drag.js
@@ -1,7 +1,7 @@
 define([
-
+  'jquery'
 ],function(
-
+  $
 ){
 
   function drag ( el, before_drag, dragging, after_drag ) {
@@ -22,13 +22,23 @@ define([
         document.body.removeEventListener('mousemove',moving,true);
         document.body.removeEventListener('mouseup',arguments.callee,true);
 
+        $(document.body).removeClass('user-select');
         typeof after_drag == 'function' && after_drag.call( el[0], drag_data );
       },true);
 
       typeof before_drag == 'function' && before_drag.call( el[0], drag_data );
+      $(document.body).addClass('user-select');
+
     });
   }
-
+  $('head').append('<style>\
+    .user-select{\
+      -webkit-user-select: none;\
+       -khtml-user-select: none;\
+         -moz-user-select: -moz-none;\
+           -o-user-select: none;\
+              user-select: none;\
+    }</style>')
   return drag;
 
 });
