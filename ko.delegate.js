342d8bf985f7b098c1e2b3fa29502695a8c6b158
Torrent Lee <torrent88410@gmail.com>
2014-08-13 18:20:15 +0800

    update

--- ko.delegate.js
+++ ko.delegate.js
@@ -5,13 +5,22 @@ define([
   $,
   ko
 ){
+  function apply_delegate ( el, modelVal ) {
+    $(el).on(modelVal.type, modelVal.selector, function(e) {
+      var $data = ko.dataFor( this );
+      modelVal.handle.call( $data, $data, e );
+    });
+  }
   ko.bindingHandlers.delegate = {
     'init' : function( el, valueAccessor ) {
       var modelVal = valueAccessor();
-      $(el).on(modelVal.type, modelVal.selector, function(e) {
-        var $data = ko.dataFor( this );
-        modelVal.handle.call( $data, $data, e );
-      })
+      if( $.isArray(modelVal) ){
+        $.each(modelVal,function(idx, modelVal) {
+          apply_delegate( el, modelVal );
+        })
+      } else {
+        apply_delegate( el, modelVal );
+      }
     }
   }
 });
